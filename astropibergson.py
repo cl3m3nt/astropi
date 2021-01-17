141d801dc26f18a39eb158cd2e4220888ca22786
clementlr <clement.leroux@gmail.com>
2021-01-17 18:27:56 +0100

    updating astropibergson & training

--- astropibergson.py
+++ astropibergson.py
@@ -1,21 +1,70 @@
 import cv2
+import csv
 import matplotlib.pyplot as plt 
+import matplotlib
 import numpy as np
 import os
+import PIL
 from PIL import Image
 from prettytable import PrettyTable
 import random
 from tqdm import tqdm
 import tensorflow as tf
 import logging
+from ephem import readtle, degree
+from exif import Image
+import reverse_geocoder as rg
+from pathlib import Path
+from time import sleep
+from datetime import datetime
 
-# Pre-requesite
+# Pre-requesite #
+# Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+# Data basepath
 base_path = os.getcwd() + '/Dataset'
+dir_path = Path(__file__).parent.resolve()
+data_file = dir_path/'data.csv'
+# Ephem ISS location
+name = "ISS (ZARYA)"
+line1 = "1 25544U 98067A   20316.41516162  .00001589  00000+0  36499-4 0  9995"
+line2 = "2 25544  51.6454 339.9628 0001882  94.8340 265.2864 15.49409479254842"
+iss = readtle(name, line1, line2)
+iss.compute()
+print(iss.sublat, iss.sublong)
 
+
+# Camera Resolution
+'''
+cam = PiCamera()
+cam.resolution = (1296,972) # Valid resolution for V1 camera
+'''
 
 # Pre-Processing Helper functions
+
+def take_photo():
+    """
+    This requires the picamera library
+    This function would replace get_data
+    """
+
+def capture(camera, image):
+    """Use 'camera' to capture an 'image' file with lat/long EXIF data."""
+    iss.compute() # Get the lat/long values from ephem
+
+    # convert the latitude and longitude to EXIF-appropriate representations
+    south, exif_latitude = convert(iss.sublat)
+    west, exif_longitude = convert(iss.sublong)
+
+    # set the EXIF tags specifying the current location
+    camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+    camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+    camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+    camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
+    # capture the image
+    camera.capture(image)
 
 def get_data(images_path):
     """
@@ -82,6 +131,34 @@ def ndvi_small_image(base_path,image_list):
         ndvi_img_list.append(ndvi_img)
     return ndvi_img_list
 
+def ndvi_rgb_image(base_path,image_list):
+    ndvi_rgb_list = []
+    ndvi_images = ndvi_small_image(base_path,image_list)
+    for i in range(0,len(image_list)):
+        ndvi_rgb = cv2.cvtColor(ndvi_images[i],cv2.COLOR_GRAY2RGB)
+        ndvi_rgb_list.append(ndvi_rgb)
+    ndvi_rgb_numpy = np.array(ndvi_rgb_list)
+    return ndvi_rgb_numpy
+
+
+# ISS Location Helper functions
+# https://support.google.com/maps/answer/18539?co=GENIE.Platform%3DDesktop&hl=fr
+def convert(angle):
+    """
+    Convert an ephem angle (degrees:minutes:seconds) to
+    an EXIF-appropriate representation (rationals)
+    e.g. '51:35:19.7' to '51/1,35/1,197/10'
+    Return a tuple containing a boolean and the converted angle,
+    with the boolean indicating if the angle is negative.
+    """
+    degrees, minutes, seconds = (float(field) for field in str(angle).split(":"))
+    exif_angle = f'{abs(degrees):.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+    return degrees < 0, exif_angle
+
+south, exif_latitude = convert(iss.sublat)
+west, exif_longitude = convert(iss.sublong)
+print(south,exif_latitude)
+print(west,exif_longitude)
 
 # Model Helper functions
 def load_model(model_name):
@@ -127,10 +204,26 @@ def main():
     """
     Main program process of Bergson Astro Pi team
     """
-
+    start_time = datetime.now()
+    logger.info(f'Starting Bergson Astro Pi team experiment at {start_time}')
+    
+    
     # Datasets to Predict on - 20s on MAC for 82 images
     logger.info("Creating Datasets")
     image_list = get_data('./Dataset')
+
+
+    # Exif Metadata
+    img0 = base_path+"/"+image_list[0]
+    with open(img0, 'rb') as image_file:
+        my_image = Image(image_file)    
+        print(my_image.has_exif)
+    '''
+    exif_data = "test"
+    im.save('imageExif.jpg',  exif=exif_data)
+    '''
+
+    # NDVI Preprocessing
     x_train_2D = np.array(ndvi_small_image(base_path,image_list))
     x_train_2D = np.expand_dims(x_train_2D,axis=3)
 
@@ -147,6 +240,28 @@ def main():
     # Get Decoded Inference results
     decoded_inference = decode_inference(predictions)
     print(decoded_inference)
+
+    # Write Prediction as CSV to disk
+    logger.info("Saving NO2 prediction")
+    with open(data_file, 'w') as f:
+        writer = csv.writer(f)
+        header = ("Date/time", "Location", "Picture Name","Predicted NO2")
+        writer.writerow(header)
+        for i in range(0,len(decoded_inference)):
+            row = (datetime.now(), "(48.856614,2.3522219)", image_list[i],decoded_inference[i])
+            writer.writerow(row)
+
+    rgb_ndvi_list = ndvi_rgb_image(base_path,image_list)
+
+    # Write NDVI Image as JPG to disk with matplotlib
+    for i in tqdm(range(0,len(rgb_ndvi_list))):
+        matplotlib.image.imsave(image_list[i]+"_ndvi"+".jpeg", rgb_ndvi_list[i])
+
+    # End of experiment
+    end_time = datetime.now()
+    logger.info(f'Finishing Bergson Astro Pi team experiment at {end_time}')
+    experiment_time = end_time - start_time
+    logger.info(f'Bergson Astro Pi team experiment run time {experiment_time} for {len(image_list)} images')
     
 # Executing main
 main()
